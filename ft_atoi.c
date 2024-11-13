8212dae1a83caac061b6f3504cc41ef04e5e1970
Johnny Varila <jvarila@student.hive.fi>
2024-11-13 14:57:29 +0200

    Fixed a missing initialization for sign

--- ft_atoi.c
+++ ft_atoi.c
@@ -20,13 +20,12 @@ int	ft_atoi(const char *nptr)
 	if (!*nptr)
 		return (0);
 	num = 0;
+	sign = 1;
 	while (*nptr == 32 || (*nptr >= 9 && *nptr <= 13))
 		nptr++;
 	if (*nptr == '-' || *nptr == '+')
 		if (*(nptr++) == '-')
 			sign = -1;
-	while (*nptr == '0')
-		nptr++;
 	while (*nptr >= '0' && *nptr <= '9')
 	{
 		num = num * 10 + *(nptr++) - '0';
