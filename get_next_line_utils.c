83f490b25eb680f31bdc96e012dec542da7aa012
Johnny Varila <jvarila@student.hive.fi>
2025-01-14 15:01:58 +0200

    Removed duplicate definitions of ft_memmove and ft_strlen from get_next_line_utils.c.

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -36,38 +36,3 @@ char	*strjoin_and_free(char *s1, char *s2)
 	free(s2);
 	return (str);
 }
-
-void	*ft_memmove(void *dest, const void *src, size_t n)
-{
-	char		*dest_char_ptr;
-	const char	*src_char_ptr;
-	size_t		i;
-
-	if (n == 0)
-		return (dest);
-	dest_char_ptr = (char *)dest;
-	src_char_ptr = (const char *)src;
-	i = 0;
-	if (src_char_ptr < dest_char_ptr)
-		while (n-- > 0)
-			dest_char_ptr[n] = src_char_ptr[n];
-	else
-	{
-		while (i < n)
-		{
-			dest_char_ptr[i] = src_char_ptr[i];
-			i++;
-		}
-	}
-	return (dest);
-}
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
-}
